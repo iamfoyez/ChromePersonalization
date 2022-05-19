a8208708f90d88828cb88dfffeb4cf3705bf893e
Foyez <iamfoyez@gmail.com>
2022-05-20 00:40:48 +0600

    changed changeTitle function

--- ChromePersonalizer.js
+++ ChromePersonalizer.js
@@ -4,15 +4,29 @@
 // @version			0.1
 // @description		Rename and change icons of specific sites
 // @author			iamfoyez
-// @match			https://*/*
-// @match			*://*/*
-// @match			*
+// @include			https://*/*
+// @include			*://*/*
+// @include			*
 // @icon			https://github.com/iamfoyez/ChromePersonalizer/raw/master/images/logo.png
 // @homepage		https://github.com/iamfoyez/ChromePersonalizer/
 // @updateURL		https://raw.githubusercontent.com/iamfoyez/ChromePersonalizer/master/ChromePersonalizer.js
 // @downloadURL		https://raw.githubusercontent.com/iamfoyez/ChromePersonalizer/master/ChromePersonalizer.js
+// @grant           unsafeWindow
+// @grant           GM_addStyle
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_xmlhttpRequest
+// @grant           GM_registerMenuCommand
+// @grant           GM_deleteValue
+// @grant           GM_listValues
+// @grant           GM_getResourceText
+// @grant           GM_getResourceURL
+// @grant           GM_log
+// @grant           GM_openInTab
+// @grant           GM_setClipboard
+// @grant           GM_info
+// @grant           GM_getMetadata
 // @run-at			document-end
-// @grant			none
 // ==/UserScript==
 
 class sites {
