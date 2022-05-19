1eb4c0a07e45680b7dee02f6782eb4f6dfb7074a
Foyez <iamfoyez@gmail.com>
2022-05-20 00:31:12 +0600

    completed title, roughly

--- ChromePersonalizer.js
+++ ChromePersonalizer.js
@@ -9,26 +9,89 @@
 // @match			*
 // @icon			https://github.com/iamfoyez/ChromePersonalizer/raw/master/images/logo.png
 // @homepage		https://github.com/iamfoyez/ChromePersonalizer/
-// @updateURL		
+// @updateURL		https://raw.githubusercontent.com/iamfoyez/ChromePersonalizer/master/ChromePersonalizer.js
+// @downloadURL		https://raw.githubusercontent.com/iamfoyez/ChromePersonalizer/master/ChromePersonalizer.js
 // @run-at			document-end
 // @grant			none
 // ==/UserScript==
 
+class sites {
+
+    #sites;
+
+    constructor() {
+        this.#get_data()
+    }
+
+    /**
+    // For when data is too big
+    async #get_data() { 
+        const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.jsonhttps://raw.githubusercontent.com/iamfoyez/ChromePersonalizer/master/sites.json';
+        const request = new Request(requestURL);
+
+        const response = await fetch(request);
+        this.#sites = response.json()
+    }
+     */
+
+    #get_data() {
+        this.sites = JSON.parse(`
+        {"PowershellDocs":{"url_prefix":"docs.microsoft.com/en-us/powershell/","custom_icon":"","new_title":"Powershell Docs"},"GitHub_test":{"url_prefix":"github.com/iamfoyez/flexplayer","custom_icon":"","new_title":"FP - new way to use yt"}}
+        `)
+    }
+
+    get(key) {
+        if (!this.is_valid(key)) return null
+        return this.sites[key]
+    }
+
+    get_keys() {
+        let all_sites = []
+        for ( const k in this.sites ) all_sites.push(k)
+        return all_sites
+    }
+
+	get_all_by_prop(propName) {
+		let all_res = {}
+		for ( const s in this.sites ) {
+			if (this.sites[s][propName]) {
+				all_res[s] = this.sites[s][propName]
+			}
+		}
+		return all_res
+	}
+    
+    has_site(name) {
+        return this.sites.hasOwnProperty(name)
+    }
+
+	get_match(url) {
+		const url_prefs = this.get_all_by_prop("url_prefix") 
+		for ( const s in url_prefs) {
+			if (url.includes(url_prefs[s])) {
+				return this.sites[s] 
+			}
+		}
+		return false;
+	}
+}
+
+function changeTitle(newTitle) {
+    document.title = newTitle
+}
+
 (function() {
     'use strict';
 
-    let sites = {
-    	Powershell_Documentation: {
-    		url_prefix: "https://docs.microsoft.com/en-us/powershell/*",
-    		icon_data_uri: ""
-    	},
-    	Github_FlexPlayer: {
-    		url_prefix: "https://github.com/iamfoyez/FlexPlayer",
-    	}
-    };
-
 	// current page url
     let url_current = window.location.href;
-	console.log(url_current)
+
+	let db = new sites()
+
+	let res = db.get_match(url_current)
+
+    if (res) {
+        changeTitle(res.new_title)
+    }
 
 })();
